895abfedf6cf76ab5972ca0812b24bbea4ab5a89
Kanav chauhan <kanavchauhan23@gmail.com>
2026-02-11 15:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 
 # Database Table
 class API(db.Model):
@@ -273,6 +269,7 @@ def rate(id):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
